f667159005373e2f189d7fa2bab05a8edf68b524
abhishekwl <nateriver210@gmail.com>
2018-09-26 00:56:27 +0530

    Stable commit

--- index.js
+++ index.js
@@ -1,31 +1,63 @@
 const ipfsApi = require('ipfs-api');
 const admin = require('firebase-admin');
-const database = admin.database().ref('ipfs');
-const express = require('express');
-const bodyParser = require('body-parser');
 const serviceAccount = require('./service_account.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://musicx-46c2d.firebaseio.com'
+});
+const databaseRef = admin.database().ref('ipfs');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const ipfs = ipfsApi('0.0.0.0');
 const port = 6942;
 const app = express();
+
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/upload', (req,res)=>{
+app.get('/', (req,res)=>res.sendFile(__dirname+'/index.html'));
+
+app.post('/upload_file', (req,res)=>{
     const uid = req.query.uid;
-    const fileUrl = req.query.url;
-    ipfs.files.add(fileUrl, (err,result)=>{
-        database.child(uid).child(result).set(fileUrl)
-        .catch(err=>sendData(err,null,req,res))
-        .then(()=>sendData(err,result,req,res));
+    const file = req.files.foo;
+    ipfs.files.add(Buffer.from(file.data), (errIpfs,result)=>{
+        console.log("RESULT: "+JSON.stringify(result));
+        result = result[0];
+        const fileName = file.name;
+        const fileSize = result.size;
+        const fileType = file.mimetype;
+        const jsonToPush = {
+            name: fileName,
+            size: fileSize,
+            type: fileType
+        };
+        file.mv(__dirname+'/data/'+uid+'_'+fileName, errMv=>{
+            databaseRef.child(uid).child(result.hash).set(jsonToPush)
+            .catch(errFb=>sendData(errFb,null,req,res))
+            .then(()=>sendData(errIpfs||errMv,result,req,res))
+        });
     });
+});
+
+app.get('/get_file', (req,res)=>{
+    const uid = req.query.uid;
+    const hash = req.query.hash;
+    databaseRef.child(uid).child(hash).once('value', snapshot=>{
+        const fileName = snapshot.val().name;
+        const fileType = snapshot.val().type;
+        const fileSize = snapshot.val().size;
+        const filePath = __dirname+'/data/'+uid+'_'+fileName;
+        res.setHeader('Content-Type',fileType);
+        res.setHeader('Content-Length',fileSize);
+        res.sendFile(filePath);
+    }).catch(err=>sendData(err,null,req,res));
 });
 
 app.listen(port, '0.0.0.0', ()=>{
     console.log('[SERVER] Listening on port '+port);
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-        databaseURL: 'https://musicx-46c2d.firebaseio.com'
-    });
 });
 
 function sendData(err,data,req,res) {
